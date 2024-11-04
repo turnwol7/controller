1a0f1b394bc57cf5c4759926b6aafea3858ee5dd
JunichiSugiura <jun.sugiura.jp@gmail.com>
2024-11-04 18:52:48 +0100

    Add send button to token page

--- packages/profile/src/components/inventory/token.tsx
+++ packages/profile/src/components/inventory/token.tsx
@@ -159,6 +159,17 @@ function ERC20() {
           </CardContent>
         </Card>
       </LayoutContent>
+
+      <LayoutFooter>
+        <Button
+          className="w-full"
+          onClick={() => {
+            console.log("send");
+          }}
+        >
+          Send
+        </Button>
+      </LayoutFooter>
     </LayoutContainer>
   );
 }
